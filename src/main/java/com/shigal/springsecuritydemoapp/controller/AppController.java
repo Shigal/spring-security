f26a6b5f48120ae30af7516c1fd6b8c3a6cb19df
Shigal <shiglaws@gmail.com>
2022-12-16 12:47:48 +0530

    added controller

---
+++ src/main/java/com/shigal/springsecuritydemoapp/controller/AppController.java
@@ -0,0 +1,17 @@
+package com.shigal.springsecuritydemoapp.controller;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping
+    public String testApp(){
+        return "Spring Security in Progress";
+    }
+}
