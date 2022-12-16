bb1e03f1be8f59f93dce035b7059fa03f7736094
Shigal <shiglaws@gmail.com>
2022-12-16 16:19:51 +0530

    hardcoded user details

--- src/main/java/com/shigal/springsecuritydemoapp/SpringSecurityDemoAppApplication.java
+++ src/main/java/com/shigal/springsecuritydemoapp/SpringSecurityDemoAppApplication.java
@@ -1,13 +1,49 @@
 package com.shigal.springsecuritydemoapp;
 
+import com.shigal.springsecuritydemoapp.model.Authority;
+import com.shigal.springsecuritydemoapp.model.User;
+import com.shigal.springsecuritydemoapp.repository.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringSecurityDemoAppApplication {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserDetailsRepository userDetailsRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityDemoAppApplication.class, args);
 	}
 
+	@PostConstruct
+	protected void init(){
+		List<Authority> authorityList = new ArrayList<>();
+		authorityList.add(createAuthority("USER", "user role"));
+		authorityList.add(createAuthority("ADMIN", "admin role"));
+
+		User user = new User();
+		user.setUsername("fifa");
+		user.setPassword(passwordEncoder.encode("qatar"));
+		user.setEnabled(true);
+		user.setAuthorities(authorityList);
+
+		userDetailsRepository.save(user);
+	}
+
+	private Authority createAuthority(String roleCode, String roleDescription){
+		Authority authority = new Authority();
+		authority.setRoleCode(roleCode);
+		authority.setRoleDescription(roleDescription);
+		return authority;
+	}
 }
